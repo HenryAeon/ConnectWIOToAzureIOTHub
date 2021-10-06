58185848acd587cfc8e2d848b57b66783e0b18c6
Henry <henrylopez@email.arizona.edu>
2021-10-06 11:30:38 -0700

    Pushing initial project with data.

---
+++ include/config.h
@@ -0,0 +1,11 @@
+// Wifi
+#define IOT_CONFIG_WIFI_SSID            "mywifi"
+#define IOT_CONFIG_WIFI_PASSWORD        "mypasswrod"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN          "XXXX.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID            "XXXX"
+#define IOT_CONFIG_DEVICE_KEY           "XXXX"
+
+// Publish 1 message every 2 seconds  
+#define TELEMETRY_FREQUENCY_MILLISECS   6000
